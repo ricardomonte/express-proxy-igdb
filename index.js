f2c8f1649cfd460699db1c76ad1cba512194056a
ricardo <ramn21@gmail.com>
2021-04-27 18:20:35 +0200

    Change in port

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ function cleanDataPlatforms(data) {
 app.use(notFound);
 app.use(errorHandle);
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening port ${port}`);
