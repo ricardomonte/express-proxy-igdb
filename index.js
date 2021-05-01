e5b3807e4dc0ba3a241252afa312f7e5eaab0955
ricardo <ramn21@gmail.com>
2021-05-01 04:26:30 +0200

    Clean data

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/games", (req, res) => {
         Authorization: `Bearer ${respons}`,
       },
       body:
-        "fields name,cover.url,release_dates,screenshots,slug,storyline,summary,url,websites; limit 33;where screenshots > 1 & cover > 1 & platforms = [48,49,6];sort rating desc;",
+        "fields name,rating,genres.name,cover.url,release_dates,slug,storyline,summary,url,websites,platforms.abbreviation; limit 40;where screenshots > 1 & cover > 1 & platforms = {48,49,6} & genres > 1 & rating > 70;sort rating desc;",
     })
       .then((data) => data.json())
       .then((json) => {
@@ -30,7 +30,7 @@ app.get("/games", (req, res) => {
   });
 });
 
-app.get("/games/images", (req, res) => {
+app.get("/games/:id", (req, res) => {
   getToken.then((response) => {
     fetch("https://api.igdb.com/v4/games", {
       method: "POST",
@@ -39,48 +39,11 @@ app.get("/games/images", (req, res) => {
         "Client-ID": process.env.CLIENT_ID,
         Authorization: `Bearer ${response}`,
       },
-      body:
-        "fields name; limit 33;where screenshots > 1 & cover > 1 & platforms = [48,49,6];sort rating desc;",
+      body: `fields name,rating,genres.name,cover.url,storyline,summary,release_dates.human,screenshots.url,platforms.abbreviation,platforms.platform_logo.url;where id = ${req.params.id};`,
     })
       .then((response) => response.json())
       .then((json) => {
-        ids = getIds(json);
-        console.log(ids);
-        fetch("https://api.igdb.com/v4/games", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Client-ID": process.env.CLIENT_ID,
-            Authorization: `Bearer ${response}`,
-          },
-          body:
-            "fields screenshots.*; limit 33;" +
-            lelele(ids) +
-            ";where screenshots > 1 & cover > 1 & platforms = [48,49,6];sort rating desc;",
-        })
-          .then((data) => data.json())
-          .then((dat) => {
-            res.json(cleanDataImages(dat));
-          });
-      });
-  });
-});
-
-app.get("/games/platforms", (req, res) => {
-  getToken.then((response) => {
-    fetch("https://api.igdb.com/v4/games", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Client-ID": process.env.CLIENT_ID,
-        Authorization: `Bearer ${response}`,
-      },
-      body:
-        "fields name,platforms.abbreviation,platforms.platform_logo.url;limit 33;where screenshots > 1 & cover > 1 & platforms = [48,49,6];sort rating desc;",
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        res.json(cleanDataPlatforms(json));
+        res.json(cleanDataOnly(json));
       });
   });
 });
@@ -98,47 +61,53 @@ function errorHandle(error, req, res, next) {
   });
 }
 
-function getIds(somethin) {
-  return somethin.map((lala) => lala.id);
-}
-
-function lelele(ids) {
-  ids.forEach((id) => {
-    return `where id = ${id.toString()}`;
-  });
-}
-
 function cleanDataAll(data) {
   return data.map((dat) => ({
     id: dat.id,
     name: dat.name,
     cover: dat.cover.url.replace("thumb", "cover_big"),
-    slug: dat.slug,
-    storyline: dat.storyline || "",
-    summary: dat.summary,
+    genre: dat.genres ? dat.genres[0].name : "",
+    rating: dat.rating,
   }));
 }
 
-function cleanDataImages(data) {
+function cleanDataOnly(data) {
   return data.map((dat) => ({
     id: dat.id,
-    screenshoot: dat.screenshots.map((screen) =>
-      screen.url.replace("thumb", "screenshot_med")
-    ),
-  }));
-}
-
-function cleanDataPlatforms(data) {
-  return data.map((dat) => ({
-    platformsName: dat.platforms.map((plat) => plat.abbreviation),
-    platformsImage: dat.platforms.map((plat) => plat.platform_logo.url),
+    name: dat.name,
+    cover: dat.cover.url.replace("thumb", "cover_big"),
+    screen1: dat.screenshots[0]
+      ? dat.screenshots[0].url.replace("thumb", "cover_big")
+      : "",
+    screen2: dat.screenshots[1]
+      ? dat.screenshots[1].url.replace("thumb", "cover_big")
+      : "",
+    screen3: dat.screenshots[2]
+      ? dat.screenshots[2].url.replace("thumb", "cover_big")
+      : "",
+    [dat.platforms[0].abbreviation]: dat.platforms[0].abbreviation,
+    [dat.platforms[1].abbreviation]: dat.platforms[1].abbreviation,
+    [dat.platforms[2].abbreviation]: dat.platforms[2].abbreviation,
+    [dat.platforms[0].abbreviation + "release"]: dat.release_dates[0].human,
+    [dat.platforms[1].abbreviation + "release"]: dat.release_dates[1].human,
+    [dat.platforms[2].abbreviation + "release"]: dat.release_dates[2].human,
+    [dat.platforms[0].abbreviation + "logo"]: dat.platforms[0].platform_logo
+      .url,
+    [dat.platforms[1].abbreviation + "logo"]: dat.platforms[1].platform_logo
+      .url,
+    [dat.platforms[2].abbreviation + "logo"]: dat.platforms[2].platform_logo
+      .url,
+    summary: dat.summary || "",
+    storyline: data.storyline || "",
+    genre: dat.genres ? dat.genres[0].name : "",
+    rating: dat.rating,
   }));
 }
 
 app.use(notFound);
 app.use(errorHandle);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`listening port ${port}`);
